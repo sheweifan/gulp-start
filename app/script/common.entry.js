99901ac08bc563777deb0679f1bba0d5348dff49
weifan she <923986234@qq.com>
2018-01-03 16:00:16 +0800

    add

---
+++ app/script/common.entry.js
@@ -0,0 +1,5 @@
+import $ from 'jquery'
+import _ from 'lodash/core'
+
+window.$ = $
+window._ = _
--- app/script/index.entry.js
+++ app/script/index.entry.js
@@ -1,15 +1,12 @@
-import a from './a'
-console.log(a)
-
+import $ from '$'
 const b = 2
 const c = 5
-
 const d = 7
-
 const f = 9
-
 const fff = 16
-
+$('body').css({
+  backgroundColor: 'red'
+})
 ;(async () => {
-  console.log(123123)
+  console.log(123)
 })()
